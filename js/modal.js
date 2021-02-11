56916acc3e426c06f6241f1d97b901c35961dcae
Alejandra Miranda <ale.miranda890@gmail.com>
2021-02-10 23:49:31 -0500

    site web fait avec Jquery

---
+++ js/modal.js
@@ -0,0 +1,22 @@
+//Script qui permet d'afficher un effet modal lorsque l'utilisateur click sur une image de la section "proyectos"
+
+$(document).ready(function () {
+
+  $('.imagen-proyecto').on('click', function () { 
+
+    var rutaImagen = $(this).attr('src');
+    var modal = '<div class="modal" id="modal"><img src="'+ rutaImagen +'" alt=""><div class="btn-cerrar" id="btnCerrar"><i class="fa fa-times" aria-hidden="true"></i></div></div>';
+    $('#proyectos').after(modal);
+
+    $('#btnCerrar').on('click', function () {
+      $('#modal').remove();
+    })
+  })
+
+  $(document).on('keyup', function (e) {
+    if (e.which==27) { 
+      $('#modal').remove();
+    }
+  })
+
+});
